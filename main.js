02679cc7ed356770ccb418681c6a3a62ea3d8286
OkadaShotaro <b1016117@fun.ac.jp>
2019-02-02 16:37:02 +0900

    アイコンの表示とコンテキストメニューの追加

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-// Electronのモジュール
-const electron = require("electron");
+const electron = require("electron"); // Electronのモジュール
+const app = electron.app;// アプリケーションをコントロールするモジュール
+const BrowserWindow = electron.BrowserWindow;// ウィンドウを作成するモジュール
+let mainWindow;// メインウィンドウはGCされないようにグローバル宣言
+const Tray = electron.Tray;// メニューバーにアイコンを表示させるために使う
+var appIcon = null// アイコン画像を入れる変数
+const Menu = electron.Menu;
 
-// アプリケーションをコントロールするモジュール
-const app = electron.app;
 
-// ウィンドウを作成するモジュール
-const BrowserWindow = electron.BrowserWindow;
 
-// メインウィンドウはGCされないようにグローバル宣言
-let mainWindow;
 
 // 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
@@ -24,13 +23,55 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({
     'width': 500,
     'height': 600,
-    'transparent': true,
-    'frame': false
+    'transparent': false, //trueで背景色を透明にできる
+    'frame': true
   });
+
+  appIcon = new Tray(__dirname + '/images/icon.jpg');
+    // コンテキストメニュー追加
+    var contextMenu = Menu.buildFromTemplate([
+      {label: '選択メニュー1', type: 'radio'},
+      {label: '選択メニュー2', type: 'radio'},
+      {type: 'separator'},
+      {label: 'サブメニュー', submenu: [
+        {label: 'サブメニュー1'},
+        {label: 'サブメニュー2'}
+        ]},
+      // {label: '終了', accelerator: 'Command+Q', click: function() { app.quit(); }}
+    ]);
+  appIcon.setContextMenu(contextMenu);
+  
+  // アイコンにマウスオーバーした時の説明
+  appIcon.setToolTip('This is sample.');
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
- 
+  // アプリケーションメニュー設定
+var menu = Menu.buildFromTemplate([
+  {
+    label: 'Sample',
+    submenu: [
+      {label: 'About'},
+      {label: 'Quit'}
+    ]
+  },
+  {
+    label: 'File',
+    submenu: [
+      {label: 'New File'},
+      {label: 'Paste'}
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {label: 'Copy', accelerator: 'Command+C', selector: 'copy'},
+      {label: 'Paste', accelerator: 'Command+V', selector: 'paste'}
+    ]
+  }
+]);
+Menu.setApplicationMenu(menu);
+
 
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function() {
